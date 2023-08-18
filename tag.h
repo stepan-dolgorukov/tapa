9cad6a06444417d731bce5c0d57ae469e04455fc
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-18 23:18:05 +0500

    Use tag structure settings in tag structure definition

--- tag.h
+++ tag.h
@@ -1,9 +1,11 @@
 #ifndef TAG_H
 #define TAG_H
 
+#include "settings.h"
+
 struct tag {
-  char name[256];
-  struct tag* children[8];
+  char name[size_name_tag_max];
+  struct tag* children[amount_children_tag_max];
 };
 
 #endif
