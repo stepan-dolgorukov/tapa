e0ff9565c9417a95dc7e72303d988940458db3ca
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-19 10:57:55 +0500

    Define "create_tag" function

---
+++ tag.c
@@ -0,0 +1,46 @@
+#include "tag.h"
+#include "settings.h"
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+int
+create_tag(
+  const char* name,
+  const struct tag* children[],
+
+  struct tag** tag_new
+) {
+
+  if (NULL == tag_new) {
+
+    return 1;
+  }
+
+  if (NULL != *tag_new) {
+
+    return 1;
+  }
+
+  *tag_new = (struct tag*)malloc(sizeof(struct tag));
+
+  if (NULL == *tag_new) {
+
+    return 1;
+  }
+
+  strncpy((*tag_new)->name, name, size_name_tag_max);
+
+  if (NULL != children) {
+
+    for (size_t index = 0u;
+      (index < amount_children_tag_max) && (NULL != children[index]);
+      ++index) {
+
+      (*tag_new)->children[index] = children[index];
+    }
+  }
+
+  return 0;
+}
