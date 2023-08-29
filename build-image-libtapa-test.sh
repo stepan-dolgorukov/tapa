68c87a2c27557e1c8a13f31f783a932302bc9866
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-29 21:41:38 +0500

    Add "latest" tag for the most fresh image

--- build-image-libtapa-test.sh
+++ build-image-libtapa-test.sh
@@ -2,8 +2,10 @@
 
 id_commit_head="$(git rev-parse --short HEAD)"
 tag_image_commit="libtapa-test:${id_commit_head}"
+tag_image_latest="libtapa-test:latest"
 
 docker build \
   -t "${tag_image_commit}" \
+  -t "${tag_image_latest}" \
   -f libtapa-test.dockerfile \
   "$(pwd)"
