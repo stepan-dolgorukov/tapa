d2cb19258cea1b526759a01686b59dc9d9eca5dc
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-29 21:34:39 +0500

    Use $(...) notation

--- build-image-libtapa-test.sh
+++ build-image-libtapa-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-id_commit_head=`git rev-parse --short HEAD`
+id_commit_head=$(git rev-parse --short HEAD)
 tag_image_libtapa_test=libtapa-test:${id_commit_head}
 
 docker build \
   -t ${tag_image_libtapa_test} \
   -f libtapa-test.dockerfile \
-  `pwd`
+  $(pwd)
