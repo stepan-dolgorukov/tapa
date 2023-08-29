a475e55e007d7aa8e62e7dc18cb670b59f90e35e
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-29 21:28:26 +0500

    Put commit HEAD hashcode to tag (after :) section

--- build-image-libtapa-test.sh
+++ build-image-libtapa-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 id_commit_head=`git rev-parse --short HEAD`
-tag_image_libtapa_test=libtapa-test-${id_commit_head}
+tag_image_libtapa_test=libtapa-test:${id_commit_head}
 
 docker build \
   -t ${tag_image_libtapa_test} \
