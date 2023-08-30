bcd31e4b06b48b5ad6d84ed8e6eefe79ea7c60c1
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-31 00:28:04 +0500

    Use "dockerfile" variable

--- build-image-libtapa-test.sh
+++ build-image-libtapa-test.sh
@@ -3,9 +3,10 @@
 id_commit_head="$(git rev-parse --short HEAD)"
 tag_image_commit="libtapa-test:${id_commit_head}"
 tag_image_latest="libtapa-test:latest"
+dockerfile="libtapa-test.dockerfile"
 
 docker build \
   -t "${tag_image_commit}" \
   -t "${tag_image_latest}" \
-  -f libtapa-test.dockerfile \
+  -f "${dockerfile}" \
   "$(pwd)"
