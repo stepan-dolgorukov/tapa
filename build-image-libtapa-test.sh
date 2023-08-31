3b05faa9f17a4435efecacdfb7c884b4694aef0b
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-08-31 08:39:17 +0500

    Use image name variable

--- build-image-libtapa-test.sh
+++ build-image-libtapa-test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 id_commit_head="$(git rev-parse --short HEAD)"
-tag_image_commit="libtapa-test:${id_commit_head}"
-tag_image_latest="libtapa-test:latest"
-dockerfile="libtapa-test.dockerfile"
+name_image="libtapa-test"
+tag_image_commit="${name_image}:${id_commit_head}"
+tag_image_latest="${name_image}:latest"
+dockerfile="${name_image}.dockerfile"
 
 docker build \
   -t "${tag_image_commit}" \
