ebe66be2261bbcbaaf66a2f56cdc5ec7ae6eaf39
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-09-02 06:08:01 +0500

    Add tag with children test

--- libtapa-test.c
+++ libtapa-test.c
@@ -29,8 +29,51 @@ test_tag_no_children(void) {
   assert(NULL == root);
 }
 
+void
+test_tag_with_children(void) {
+
+  printf("%s\n", __FUNCTION__);
+
+  struct tag *root, *left, *center, *right;
+
+  int status;
+  size_t index;
+
+  root = left = center = right = NULL;
+  status = create_tag("root", &root);
+
+  assert(0 == status);
+  assert(0 == strcmp("root", root->name));
+
+  status = create_tag("left", &left);
+  assert(0 == status);
+  assert(0 == strcmp("left", left->name));
+
+  status = append_child(left, root);
+  assert(0 == status);
+
+  status = create_tag("center", &center);
+  assert(0 == status);
+  assert(0 == strcmp("center", center->name));
+
+  status = append_child(center, root);
+  assert(0 == status);
+
+  status = create_tag("right", &right);
+  assert(0 == status);
+  assert(0 == strcmp("right", right->name));
+
+  status = append_child(right, root);
+  assert(0 == status);
+
+  delete_tag(&root);
+
+  assert(NULL == root);
+}
+
 int main(void) {
   test_tag_no_children();
+  test_tag_with_children();
 
   return 0;
 }
