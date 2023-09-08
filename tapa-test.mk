769ccc6a0144546abf6b2063bb315548203ccbaf
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-09-09 00:19:41 +0500

    Correction of targets names

--- tapa-test.mk
+++ tapa-test.mk
@@ -5,7 +5,7 @@ ID_COMMIT_HEAD != git rev-parse --short HEAD
 TAG_IMAGE_COMMIT := $(NAME_IMAGE):$(ID_COMMIT_HEAD)
 TAG_IMAGE_LATEST := $(NAME_IMAGE):latest
 
-image:
+image-$(NAME_IMAGE):
 	docker build \
 	--tag $(TAG_IMAGE_COMMIT) \
 	--tag $(TAG_IMAGE_LATEST) \
