2dd05d1afb6d5a1f2ac3441385bb470386addfd9
Stepan Dolgorukov <stepan.dolgorukov@vk.com>
2023-10-02 20:47:55 +0500

    Use size_value_tag_max from settings
    diff --git a/value_tag.h b/value_tag.h
    index 9b8ec59..be08375 100644
    --- a/value_tag.h
    +++ b/value_tag.h
    @@ -1,6 +1,7 @@
     #ifndef VALUE_H
     #define VALUE_H
    
    +#include "settings.h"
     #include "type_value.h"
    
     struct value_tag {
    @@ -9,7 +10,7 @@ struct value_tag {
       union {
         int integer;
         float floating_point;
    -    char string[80];
    +    char string[size_value_tag_max];
       } value;
     };

--- value_tag.h
+++ value_tag.h
@@ -1,6 +1,7 @@
 #ifndef VALUE_H
 #define VALUE_H
 
+#include "settings.h"
 #include "type_value.h"
 
 struct value_tag {
@@ -9,7 +10,7 @@ struct value_tag {
   union {
     int integer;
     float floating_point;
-    char string[80];
+    char string[size_value_tag_max];
   } value;
 };
 
